070af4800b0ba7f2a194308d2f83896c82876478
Subham Sharma <subhamsharma@Subhams-MacBook-Pro.local>
2023-05-21 18:07:43 +0530

    final launch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,26 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import "../styles/footer.css";
+import {Link} from "react-router-dom";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon /> 
-        <TwitterIcon /> 
-        <FacebookIcon /> 
-        <LinkedInIcon />
+        <Link to="https://www.instagram.com/">
+          <InstagramIcon />  
+        </Link> 
+        <Link to="https://www.twitter.com/">
+          <TwitterIcon /> 
+        </Link>
+        <Link to="https://www.facebook.com/">
+          <FacebookIcon /> 
+        </Link>
+        <Link to="https://www.linkedin.com/">
+          <LinkedInIcon />
+        </Link>
       </div>
-      <p> &copy; 2021 pedrotechpizza.com</p>
+      <p> &copy; 2023 gofoodpizza.com</p>
     </div>
   );
 }
